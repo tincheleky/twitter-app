76fdffb5475ece1c8fa31afd72d943c209e5f4e6
tincheleky <bebindenhat@yahoo.com>
2017-04-24 11:07:26 -0700

    Remove keys

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   twitter: {
-    consumer_key: 'wRqSFCO1SMdeMpxZCSKpiMEiI',
-    consumer_secret: '9oTFECTMIc8zS5DXTTzdldoNOZ7wJNp9cJp1lcVgFtuAG0rcQs',
-    access_token_key: '847169505507753984-Fbm5Q8juFKNSSBA4SStE5ch0nLVqVuC',
-    access_token_secret: 'gC2jWzOAuO7CW92EBORzparCRRusZsYtUNEweLXqhJWJd',
+    consumer_key: 'Your consumer key',
+    consumer_secret: 'your consumer secret key',
+    access_token_key: 'your token key',
+    access_token_secret: 'your token secret key',
   }
 }
